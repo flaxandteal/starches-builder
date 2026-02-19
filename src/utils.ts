166ca52f809d733f5660c67b0c5eaab927ead30e
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2026-02-19 15:05:37 +0000

    fix: handlebars import

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from "fs";
-import * as Handlebars from 'handlebars';
+import Handlebars from 'handlebars';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { validatePathWithinBase } from './safe-utils';
